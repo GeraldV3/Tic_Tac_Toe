20d4ef8adad51db2432729b323abafa42f1755fc
GeraldV3 <lopezgerald981@gmail.com>
2022-05-31 18:18:18 +0800

    Start button is working. And I added a new form for Ingame Tic Tac Toe

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Tic_Tac_Toe
 {
-    partial class Form1
+    partial class Lobby
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,40 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Lobby));
+            this.Start_btn = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Start_btn)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Start_btn
+            // 
+            this.Start_btn.BackgroundImage = global::Tic_Tac_Toe.Properties.Resources.Start;
+            this.Start_btn.Location = new System.Drawing.Point(107, 130);
+            this.Start_btn.Name = "Start_btn";
+            this.Start_btn.Size = new System.Drawing.Size(380, 100);
+            this.Start_btn.TabIndex = 0;
+            this.Start_btn.TabStop = false;
+            this.Start_btn.Click += new System.EventHandler(this.Start_btn_Click);
+            this.Start_btn.MouseLeave += new System.EventHandler(this.Start_btn_MouseLeave);
+            this.Start_btn.MouseHover += new System.EventHandler(this.Start_btn_MouseHover);
+            // 
+            // Lobby
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackgroundImage = global::Tic_Tac_Toe.Properties.Resources.Lobby;
+            this.ClientSize = new System.Drawing.Size(584, 561);
+            this.Controls.Add(this.Start_btn);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Lobby";
+            this.Text = "Lobby";
+            ((System.ComponentModel.ISupportInitialize)(this.Start_btn)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private PictureBox Start_btn;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,28 @@
+using System.Media;
 namespace Tic_Tac_Toe
 {
-    public partial class Form1 : Form
+    public partial class Lobby : Form
     {
-        public Form1()
+        SoundPlayer StartSound = new SoundPlayer(@"C:\Users\pc\Desktop\Tic Tac Toe\Picture and Sound\Pokemon.wav");
+        public Lobby()
         {
             InitializeComponent();
         }
-    }
+
+        private void Start_btn_MouseHover(object sender, EventArgs e)
+        {
+            Start_btn.Image = Properties.Resources.Start1;
+        }
+        private void Start_btn_MouseLeave(object sender, EventArgs e)
+        {
+            Start_btn.Image = Properties.Resources.Start;
+        }
+
+        private void Start_btn_Click(object sender, EventArgs e)
+        {
+            StartSound.Play();
+            InGame Start = new InGame();
+            Start.ShowDialog();
+        }
+    } 
 }
